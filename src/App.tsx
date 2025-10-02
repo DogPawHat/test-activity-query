0f3e27f26ff80828638a1e0bf03da17fc2ee478c
Ciarán Curley <ciaran@alludium.ai>
2025-10-02 22:27:15 +0200

    stuff

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { Card, CardContent } from "./components/ui/card";
+import { Button } from "./components/ui/button";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <main className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold">Vite + React</h1>
+      <Card>
+        <CardContent>
+          <Button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </Button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </CardContent>
+      </Card>
+    </main>
+  );
+}
+
+export default App;
