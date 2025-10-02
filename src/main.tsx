083998879755075cf98238176fa1d95c8566aec2
Ciarán Curley <ciaran@alludium.ai>
2025-10-03 00:03:31 +0200

    query

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtoolsPanel } from "@tanstack/react-query-devtools";
-import { TanStackDevtools } from "@tanstack/react-devtools";
 import "./index.css";
 import App from "./app.tsx";
+
+const ReactQueryDevtoolsProduction = React.lazy(() =>
+  import("@tanstack/react-query-devtools/build/modern/production.js").then(
+    (d) => ({
+      default: d.ReactQueryDevtools,
+    })
+  )
+);
 
 if (!document.documentElement.classList.contains("dark")) {
   document.documentElement.classList.add("dark");
@@ -16,14 +22,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <TanStackDevtools
-        plugins={[
-          {
-            name: "TanStack Query",
-            render: <ReactQueryDevtoolsPanel />,
-          },
-        ]}
-      />
+      <ReactQueryDevtoolsProduction />
     </QueryClientProvider>
   </StrictMode>
 );
